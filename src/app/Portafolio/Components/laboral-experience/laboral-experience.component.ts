e40676baa2a27dfce63084c4709eb865ee95b00f
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-07 11:34:47 -0300

    feat: infromacion laboral creada

---
+++ src/app/Portafolio/Components/laboral-experience/laboral-experience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'laboral-experience',
+  imports: [],
+  templateUrl: './laboral-experience.component.html',
+  styleUrl: './laboral-experience.component.css'
+})
+export class LaboralExperienceComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { HeaderComponent } from './Portafolio/Components/header/header.component';
+import { LaboralExperienceComponent } from './Portafolio/Components/laboral-experience/laboral-experience.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, LaboralExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
