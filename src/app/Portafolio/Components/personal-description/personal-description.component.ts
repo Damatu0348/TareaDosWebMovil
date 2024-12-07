8553c8357eef3309bd6e22e9b5eee0ac5614213e
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-07 15:48:32 -0300

    feat: informacion personal añadida

---
+++ src/app/Portafolio/Components/personal-description/personal-description.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'personal-description',
+  imports: [],
+  templateUrl: './personal-description.component.html',
+  styleUrl: './personal-description.component.css'
+})
+export class PersonalDescriptionComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { HeaderComponent } from './Portafolio/Components/header/header.component';
 import { LaboralExperienceComponent } from './Portafolio/Components/laboral-experience/laboral-experience.component';
+import { PersonalDescriptionComponent } from './Portafolio/Components/personal-description/personal-description.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LaboralExperienceComponent],
+  imports: [HeaderComponent, LaboralExperienceComponent, PersonalDescriptionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
