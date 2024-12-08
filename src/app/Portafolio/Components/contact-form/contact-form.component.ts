b6b2af70dbf3994601c10eebccf00406bccc7325
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-08 16:23:04 -0300

    fix: form de contacto mejorado e implementado

--- src/app/Portafolio/Components/contact-form/contact-form.component.ts
+++ src/app/Portafolio/Components/contact-form/contact-form.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'contact-form',
-  imports: [],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  email: string = '';
+  message: string = '';
+  isSending: boolean = false;
 
+  submitForm() {
+    if (!this.email || !this.message) {
+      alert('Por favor completa todos los campos.');
+      return;
+    }
+
+    // Mostrar indicador "Enviando..."
+    this.isSending = true;
+
+    // Simular envío del formulario
+    setTimeout(() => 
+      {
+        this.isSending = false;
+        alert('Formulario enviado correctamente');
+        
+        // Reiniciar los campos (tiempo de espera: 2 segundos)
+        this.email = '';
+        this.message = '';
+      }, 
+      2000); 
+  }
 }
