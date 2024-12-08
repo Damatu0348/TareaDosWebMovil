9498798c827ed191793e68c9ee5bad30fa1d694e
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-07 22:30:26 -0300

    feat: formulario de contacto creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { initFlowbite } from 'flowbite';
 import { HeaderComponent } from './Portafolio/Components/header/header.component';
 import { LaboralExperienceComponent } from './Portafolio/Components/laboral-experience/laboral-experience.component';
 import { PersonalDescriptionComponent } from './Portafolio/Components/personal-description/personal-description.component';
+import { ContactFormComponent } from './Portafolio/Components/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LaboralExperienceComponent, PersonalDescriptionComponent],
+  imports: [HeaderComponent, LaboralExperienceComponent, PersonalDescriptionComponent, ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
